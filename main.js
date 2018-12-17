586c369f7befd716f360d17336ed8c3e9fc977fe
izmr <macbookpro@MacBook-no-MacBook-Pro.local>
2018-12-17 21:33:23 +0900

    一覧表示まで実装

--- main.js
+++ main.js
@@ -32,26 +32,38 @@
   //    - ul要素にli要素を追加して、li要素内にtodoタスクの内容を表示する
   //    - li要素内に削除ボタンを配置して、削除ボタンをクリックしたら対応するタスクを削除する
 
-  const showToDos = function() {
+  const showToDos = function () {
     // 表示しているulToDoListの子要素を一旦削除する
-    while(ulToDoList.firstChild) {
+    while (ulToDoList.firstChild) {
       ulToDoList.removeChild(ulToDoList.firstChild)
     }
     // toDos内の要素をHTMLに全て表示する
     toDos.forEach(function (item, index) {
       const liElementForToDo = document.createElement('li');
       const num = index + 1;
-      liElementForToDo.textContent = num + ' : ' + item;
+      liElementForToDo.innerHTML = num + ' : ' + item + '  <input type="button" class="delete-toDo" value="削除"> ';
+      liElementForToDo.id = index;
       ulToDoList.appendChild(liElementForToDo);
     })
   };
-
-
 
 
   // Todo情報を表すli要素(showTodo関数で作成される要素)の中にある削除ボタンをクリックしたら実行される。
   //   - todosから対応するtodo情報を削除する
   //   - 引数はindexを受け取る(インデックス番号)
   //   - 削除後はshowTodosを実行して、Todoリストを整理する
+
+  // 削除ボタンのDOM取得
+  const deleteButton = document.getElementById('delete-toDo-button');
+
+  // 削除するToDoのDOMを取得
+  const deletedToDo = document.getElementById(index);
+
+  // 削除ボタンを押した時の処理を実装
+  deleteButton.addEventListener(click, (event => {
+
+
+    showToDos();
+  }));
 
 })();
